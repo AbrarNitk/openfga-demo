cfaa73a019d1db7a8c4f3a152b3efc4e77d40496
AbrarNitk <abrar.nitk@gmail.com>
2025-09-04 16:18:48 +0530

    added in the envss

--- etc/envs.sh
+++ etc/envs.sh
@@ -1,3 +1,4 @@
 export PROFILE=dev
 export DATABASE_URL=postgres://openfga_user:password@localhost:5432/openfga?sslmode=disable
 export OPENFGA_CLIENT_URL=http://localhost:8080
+export OPENFGA_STORE_ID=01K47J3VCGSSC57S41QES6KZNB
